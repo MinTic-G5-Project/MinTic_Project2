d9b453a34a5bd13d1f8ead8ba345b89b777526bb
adrian <ing.control.adrian@gmail.com>
2022-09-01 13:25:44 -0700

    Creacion de clase main y comprobacion de todo

--- src/main/java/com/example/Proyecto1/Proyecto1Application.java
+++ src/main/java/com/example/Proyecto1/Proyecto1Application.java
@@ -4,7 +4,17 @@ package com.example.Proyecto1;
 public class Proyecto1Application {
 
 	public static void main(String[] args) {
+		Finca finca1= new Finca("El palomo", "3213178899");
+		Finca finca2= new Finca("El palomo", "3213178899");
 
+		Sensor sensor1= new Sensor(15,finca1);
+		Sensor sensor2= new Sensor(30,finca2);
+		Sensor sensor3= new Sensor(45,finca2);
+
+		MovimientoSensor mov1= new MovimientoSensor(sensor1, 20);
+
+		System.out.println(sensor1.getFinca().getNombre());
+		System.out.println(sensor1.getPresicion());
 
 	}
 
